1e0bf2fe4304a6273126c259fad35f9414f1d134
Juan C Martinez <martinezjuan057@gmail.com>
2024-01-26 16:41:47 -0600

    Protocol settings abstracted and no lonnger hardcoded.

--- main.py
+++ main.py
@@ -10,17 +10,11 @@ f.close()
 # TODO: Make a function that retrieves the JSON and feed it to the zipa system.
 IP = ni.ifaddresses("eth0")[ni.AF_INET][0]["addr"]  # <--- device's ip address goes here
 PORT = 5005
-SAMPLE_RATE = 44100
-SECONDS = 30
-N = 16
-K = 4
 SERVICE_NAME = "_zipa._tcp.local."
-TIMEOUT = 10
 
 UNCOND_HOST = True
 
 if __name__ == "__main__":
     zipa_sys = ZIPA_System(
-        ID, IP, PORT, SERVICE_NAME, "/mnt/data", TIMEOUT, SAMPLE_RATE, SECONDS, N, K
-    )
+        ID, IP, PORT, SERVICE_NAME, "/mnt/data")
     zipa_sys.start()
