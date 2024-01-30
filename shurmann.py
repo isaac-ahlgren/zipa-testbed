71f2c49d586a015f0d601e8830a92ba2c5ef84be
Juan C Martinez <jmartinez29@luc.edu>
2024-01-30 17:01:46 -0600

    Cleaning up before merge.

--- shurmann.py
+++ shurmann.py
@@ -1,14 +1,14 @@
-import numpy as np
 import mysql.connector
-from microphone import Microphone
+import numpy as np
+
 from corrector import Fuzzy_Commitment
 from galois import *
-# from network import *
-from network_json import * 
+from network import *
 
-class Shurmann_Siggs_Protocol():
+
+class Shurmann_Siggs_Protocol:
     def __init__(self, microphone, n, k, timeout, nfs_server_dir, identifier):
-        self.signal_measurement = microphone 
+        self.signal_measurement = microphone
         self.re = Fuzzy_Commitment(n, k)
         self.name = "shurmann-siggs"
         self.count = 0
@@ -16,41 +16,41 @@ class Shurmann_Siggs_Protocol():
         self.nfs_server_dir = nfs_server_dir
         self.identifier = identifier
 
-    def sigs_algo(self, x1,window_len=10000, bands=1000):
+    def sigs_algo(self, x1, window_len=10000, bands=1000):
         FFTs = []
-        from scipy.fft import fft, fftfreq, ifft, rfft, irfft
+        from scipy.fft import fft, fftfreq, ifft, irfft, rfft
 
         if window_len == 0:
             window_len = len(x)
 
         x = np.array(x1.copy())
-        #wind = scipy.signal.windows.hann(window_len)
-        for i in range(0,len(x),window_len):
-        
-            if len(x[i:i+window_len]) < window_len:
-                #wind = scipy.signal.windows.hann(len(x[i:i+window_len]))
-                x[i:i+window_len] = x[i:i+window_len] #* wind
+        # wind = scipy.signal.windows.hann(window_len)
+        for i in range(0, len(x), window_len):
+            if len(x[i : i + window_len]) < window_len:
+                # wind = scipy.signal.windows.hann(len(x[i:i+window_len]))
+                x[i : i + window_len] = x[i : i + window_len]  # * wind
             else:
-                x[i:i+window_len] = x[i:i+window_len] #* wind
+                x[i : i + window_len] = x[i : i + window_len]  # * wind
 
-            FFTs.append(abs(rfft(x[i:i+window_len])))
- 
+            FFTs.append(abs(rfft(x[i : i + window_len])))
+
         E = {}
         bands_lst = []
-        for i in range(0,len(FFTs)):
+        for i in range(0, len(FFTs)):
             frame = FFTs[i]
-            bands_lst.append([ frame[k:k+bands] for k in range(0,len(frame),bands)])
-            for j in range(0,len(bands_lst[i])):
-                E[(i,j)] = np.sum(bands_lst[i][j])
+            bands_lst.append(
+                [frame[k : k + bands] for k in range(0, len(frame), bands)]
+            )
+            for j in range(0, len(bands_lst[i])):
+                E[(i, j)] = np.sum(bands_lst[i][j])
 
         bs = ""
-        for i in range(1,len(FFTs)):
-            for j in range(0,len(bands_lst[i])-1):
-            
-                if E[(i,j)] -E[(i,j+1)] - (E[(i-1,j)] - E[(i-1,j+1)]) > 0:
-                    bs+= "1"
+        for i in range(1, len(FFTs)):
+            for j in range(0, len(bands_lst[i]) - 1):
+                if E[(i, j)] - E[(i, j + 1)] - (E[(i - 1, j)] - E[(i - 1, j + 1)]) > 0:
+                    bs += "1"
                 else:
-                    bs+="0"   
+                    bs += "0"
         return bs
 
     def extract_context(self):
@@ -63,7 +63,7 @@ class Shurmann_Siggs_Protocol():
     def device_protocol(self, host):
         host.setblocking(1)
         print(f"Iteration {str(self.count)}.\n")
-            
+
         # Sending ack that they are ready to begin
         print("Sending ACK.\n")
         ack(host)
@@ -77,7 +77,7 @@ class Shurmann_Siggs_Protocol():
         # Extract bits from mic
         print("Extracting context\n")
         witness, signal = self.extract_context()
-        
+
         # Wait for Commitment
         print("Waiting for commitment from host")
         commitment, h = commit_standby(host, self.timeout)
@@ -110,7 +110,7 @@ class Shurmann_Siggs_Protocol():
     def host_protocol(self, device_sockets):
         print("Iteration " + str(self.count))
         print()
-  
+
         participating_sockets = ack_all_standby(device_sockets, self.timeout)
 
         # Exit early if no devices to pair with
@@ -120,7 +120,6 @@ class Shurmann_Siggs_Protocol():
             return
         print("Successfully ACKed participating devices")
         print()
-        
 
         print("ACKing all participating devices")
         ack_all(participating_sockets)
@@ -151,10 +150,38 @@ class Shurmann_Siggs_Protocol():
     def send_to_nfs_server(self, signal_type, signal, witness, h, commitment):
         root_file_name = self.nfs_server_dir + "/" + signal_type
 
-        signal_file_name = root_file_name + "_signal_id" + str(self.identifier) + "_it" + str(self.count) + ".csv"
-        witness_file_name = root_file_name + "_witness_id" + str(self.identifier) + "_it" + str(self.count) + ".txt"
-        hash_file_name = root_file_name + "_hash_id" + str(self.identifier) + "_it" + str(self.count) + ".txt"
-        commitment_file_name = root_file_name + "_commitment_id" + str(self.identifier) + "_it" + str(self.count) + ".csv"
+        signal_file_name = (
+            root_file_name
+            + "_signal_id"
+            + str(self.identifier)
+            + "_it"
+            + str(self.count)
+            + ".csv"
+        )
+        witness_file_name = (
+            root_file_name
+            + "_witness_id"
+            + str(self.identifier)
+            + "_it"
+            + str(self.count)
+            + ".txt"
+        )
+        hash_file_name = (
+            root_file_name
+            + "_hash_id"
+            + str(self.identifier)
+            + "_it"
+            + str(self.count)
+            + ".txt"
+        )
+        commitment_file_name = (
+            root_file_name
+            + "_commitment_id"
+            + str(self.identifier)
+            + "_it"
+            + str(self.count)
+            + ".csv"
+        )
 
         np.savetxt(signal_file_name, signal)
         np.savetxt(commitment_file_name, np.array(commitment.coeffs))
@@ -165,11 +192,23 @@ class Shurmann_Siggs_Protocol():
 
         conn = None
         try:
-            conn = mysql.connector.connect(user='luke', password='lucor011&', host='10.17.29.18', database='file_log')
+            conn = mysql.connector.connect(
+                user="luke",
+                password="lucor011&",
+                host="10.17.29.18",
+                database="file_log",
+            )
             cursor = conn.cursor()
 
-            for file_name in [signal_file_name, witness_file_name, hash_file_name, commitment_file_name]:
-                cursor.execute("INSERT INTO file_paths (file_path) VALUES (%s)", (file_name,))
+            for file_name in [
+                signal_file_name,
+                witness_file_name,
+                hash_file_name,
+                commitment_file_name,
+            ]:
+                cursor.execute(
+                    "INSERT INTO file_paths (file_path) VALUES (%s)", (file_name,)
+                )
 
             conn.commit()
         except mysql.connector.Error as err:
@@ -178,7 +217,3 @@ class Shurmann_Siggs_Protocol():
             if conn and conn.is_connected():
                 cursor.close()
                 conn.close()
-
-
-
-
