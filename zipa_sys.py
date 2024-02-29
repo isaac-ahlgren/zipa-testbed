746b3b4c7665d34b5069e801a8d6bdb65f4cb6f9
isaac-ahlgren <isaacahlgren12@gmail.com>
2024-02-29 13:42:55 -0600

    adding chunk sizes

--- zipa_sys.py
+++ zipa_sys.py
@@ -1,24 +1,37 @@
-import socket
-import select
 import json
+import os
+import select
+import socket
 from multiprocessing import Process
+
 import yaml
 
+from bmp280 import BMP280Sensor
 from browser import ZIPA_Service_Browser
 from microphone import Microphone
+from miettinen import Miettinen_Protocol
 from network import *
-from shurmann import Shurmann_Siggs_Protocol
+from PIR import PIRSensor
 from sensor_reader import Sensor_Reader
+from shurmann import Shurmann_Siggs_Protocol
+from test_sensor import Test_Sensor
+from VEML7700 import LightSensor
 
 # Used to initiate and begin protocol
 HOST = "host    "
 STRT = "start   "
+
 
 class ZIPA_System:
     def __init__(self, identity, ip, port, service, nfs):
         # Object is created and filled with identifying info as well as setup information
         self.id = identity
         self.nfs = nfs
+
+        time_to_collect, sample_rates, chunk_sizes = self.get_sensor_configs(
+            os.getcwd() + "/sensor_config.yaml"
+        )
+        self.create_sensors(sample_rates, time_to_collect)
 
         # Set up a listening socket
         self.ip = ip
@@ -39,7 +52,7 @@ class ZIPA_System:
         # Set up protocol and associated processes
         self.protocol_threads = []
         self.protocols = []
-
+       
 
     def start(self):
         print("Starting browser thread.\n")
@@ -78,11 +91,11 @@ class ZIPA_System:
     def service_request(self, data, incoming):
         # Retrieve command, JSON object size, JSON object
         command = data.decode()
-        length = int.from_bytes(incoming.recv(4), byteorder='big')
+        length = int.from_bytes(incoming.recv(4), byteorder="big")
         parameters = json.loads(incoming.recv(length))
-        self.timeout = parameters['timeout']
-        self.duration = parameters['duration']
-        self.sampling = parameters['sampling']
+        self.timeout = parameters["timeout"]
+        self.duration = parameters["duration"]
+        self.sampling = parameters["sampling"]
         # Create protocol based on JSON
         self.create_protocol(parameters)
 
@@ -93,11 +106,13 @@ class ZIPA_System:
 
             for protocol in self.protocols:
                 # Find the protocol that the message demands
-                if protocol.name == parameters['protocol']['name']:
+                if protocol.name == parameters["protocol"]["name"]:
                     participants = self.initialize_protocol(parameters)
 
                     if len(participants) == 0:
-                        print("No discoverable devices to perform protocol. Aborting.\n")
+                        print(
+                            "No discoverable devices to perform protocol. Aborting.\n"
+                        )
                         return False
 
                     # Run the process in the background
@@ -109,7 +124,7 @@ class ZIPA_System:
             print("Device selected as a client.")
 
             for protocol in self.protocols:
-                if protocol.name == parameters['protocol']['name']:
+                if protocol.name == parameters["protocol"]["name"]:
                     thread = Process(target=protocol.device_protocol, args=[incoming])
                     thread.start()
 
@@ -118,10 +133,12 @@ class ZIPA_System:
                     self.protocol_threads.append(thread)
 
     def initialize_protocol(self, parameters):
-        print(f"Initializing {parameters['protocol']['name']} protocol on all participating devices.")
-        bytestream = json.dumps(parameters).encode('utf8')
-        length = len(bytestream).to_bytes(4, byteorder='big')
-        message = (STRT.encode() + length + bytestream)
+        print(
+            f"Initializing {parameters['protocol']['name']} protocol on all participating devices."
+        )
+        bytestream = json.dumps(parameters).encode("utf8")
+        length = len(bytestream).to_bytes(4, byteorder="big")
+        message = STRT.encode() + length + bytestream
         candidates = self.browser.get_ip_addrs_for_zipa()
         participants = []
         print(f"Potential participants: {str(candidates)}")
@@ -144,34 +161,72 @@ class ZIPA_System:
                 print(f"Error connecction to {candidate}. Error: {str(failed)}.\n")
 
         return participants
-    
+
     def create_protocol(self, parameters):
-        name = parameters['protocol']['name']
+        name = parameters["protocol"]["name"]
 
         match name:
             case "shurmann-siggs":
-                self.protocols.append(Shurmann_Siggs_Protocol(
-                    Microphone(self.sampling, int(self.duration * self.sampling)),
-                    parameters['protocol']['n'],
-                    parameters['protocol']['k'],
-                    self.timeout,
-                    self.nfs,
-                    self.id
-                ))
-            #TODO: make it so miettinen can be called (but the signal processing algorithm for it should be tested first)
-            #case "miettinen":
-            #    self.protocols.append(Miettinen_Protocol(
-            #        
-            #    ))
+                self.protocols.append(
+                    Shurmann_Siggs_Protocol(
+                        self.sensors[parameters["sensor"]],
+                        parameters["n"],
+                        parameters["k"],
+                        self.timeout,
+                        self.nfs,
+                        self.id,
+                    )
+                )
+
+            case "miettinen":
+                self.protocols.append(
+                    Miettinen_Protocol(
+                        self.sensors[parameters["sensor"]],
+                        8,  # TODO Rework once shurmann has parameterized key length
+                        parameters["n"],
+                        parameters["k"],
+                        parameters["protocol"]["f"],
+                        parameters["protocol"]["w"],
+                        parameters["protocol"]["rel_thresh"],
+                        parameters["protocol"]["abs_thresh"],
+                        parameters["protocol"]["auth_thesh"],
+                        parameters["protocol"]["success_thresh"],
+                        parameters["protocol"]["max_iterations"],
+                        self.timeout,
+                    )
+                )
             case _:
                 print("Protocol not supported.")
 
     def get_sensor_configs(self, yaml_file):
-        with open(f'{yaml_file}','r') as f:
+        with open(f"{yaml_file}", "r") as f:
             config_params = yaml.safe_load(f)
-        time_to_collect = config_params['time_collected']
-        sensor_sample_rates = config_params['sensor_sample_rates']
-        return time_to_collect, sensor_sample_rates
+        time_to_collect = config_params["time_collected"]
+        sensor_sample_rates = config_params["sensor_sample_rates"]
+        chunk_sizes = config_params["chunk_sizes"]
+        return time_to_collect, sensor_sample_rates, chunk_sizes
 
+    def create_sensors(self, sample_rates, time_length, chunk_sizes):
+        # ASSUME ORDER: Mic, BMP, PIR, VEML, TEST_SENSOR
+        # Create instances of physical sensors
+        self.devices = {}
+        self.sensors = {}
+        self.devices["microphone"] = Microphone(
+            sample_rates["microphone"], sample_rates["microphone"] * time_length, chunk_sizes["microphone"]
+        )
+        self.devices["bmp280"] = BMP280Sensor(
+            sample_rates["bmp280"], sample_rates["bmp280"] * time_length, chunk_sizes["bmp280"]
+        )
+        self.devices["pir"] = PIRSensor(
+            sample_rates["pir"], sample_rates["pir"] * time_length, chunk_sizes["pir"]
+        )
+        self.devices["veml7700"] = LightSensor(
+            sample_rates["veml7700"], sample_rates["veml7700"] * time_length, chunk_sizes["veml7700"]
+        )
+        self.devices["test_sensor"] = Test_Sensor(
+            sample_rates["test_sensor"], sample_rates["test_sensor"] * time_length, chunk_sizes["test_sensor"]
+        )
 
-
+        # Wrap physical sensors into sensor reader
+        for device in self.devices:
+            self.sensors[device] = Sensor_Reader(self.devices[device])
