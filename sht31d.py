00e4a100134d333054ce1cd65045ec66660e8dd6
Juan C Martinez <jmartinez29@luc.edu>
2024-03-07 15:02:03 -0600

    Added humidity sensor. Tested individually; need to integrate with zipa_sys.py.

---
+++ sht31d.py
@@ -0,0 +1,51 @@
+import time
+
+import adafruit_sht31d
+import board
+import numpy as np
+
+from sensor_interface import SensorInterface
+
+class HumiditySensor(SensorInterface):
+    def __init__(self, sample_rate, buffer_size, chunk_size):
+        SensorInterface.__init__(self)
+        self.sample_rate = sample_rate
+        self.buffer_size = buffer_size
+        self.chunk_size = chunk_size
+        self.chunks = int(self.buffer_size / self.chunk_size)
+        self.name = "humidity"
+        self.buffer = np.zeros(chunk_size, np.float32())
+        self.buffer_index = 0
+        self.buffer_full = False
+        self.data_type = self.buffer.dtype
+        self.sensor = adafruit_sht31d.SHT31D(board.I2C())
+        self.start_thread()
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def read(self):
+        data = np.empty(self.chunk_size, self.data_type)
+
+        for i in range(self.chunk_size):
+            humidity = self.sensor.relative_humidity
+            data[i] = np.float32(humidity)
+            time.sleep(1 / self.sample_rate)
+
+        return data
+    
+if __name__ == "__main__":
+    from sensor_reader import Sensor_Reader
+    import time
+    sht31d = HumiditySensor(40, 40 * 5, 8)
+    sr = Sensor_Reader(sht31d)
+    time.sleep(3)
+    print("Beginning reading.")
+    for i in range(10):
+        results = sr.read(40 * 5)
+        print(f"Number of results: {len(results)},\n {results}")
+        time.sleep(10)
+    exit()
