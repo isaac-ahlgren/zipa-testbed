9437ad1d638f2cb804a26eb5d857e4d6a4a63c93
isaac-ahlgren <isaacahlgren12@gmail.com>
2024-03-07 21:16:01 -0600

    finished refactoring shurmann: STILL NEEDS NFS TO BE IMPLEMENTED

--- network.py
+++ network.py
@@ -34,23 +34,31 @@ def ack_standby(connection, timeout):
 
 #TODO: make it so that the size of the hash can be variable
 def send_commit(commitment, hash, device):
-    length = len(commitment).to_bytes(4, byteorder='big')
-    message = (COMM.encode() + hash + length + commitment)
+    com_length = len(commitment).to_bytes(4, byteorder='big')
+    hash_length = len(hash).to_bytes(4, byteorder='big')
+    message = (COMM.encode() + hash_length + hash + com_length + commitment)
     device.send(message)
 
 def commit_standby(connection, timeout):
     reference = time.time()
     timestamp = reference
+    commitment = None
+    hash = None
 
     while (timestamp - reference) < timeout:
         timestamp = time.time()
         # 8 byte command, 64 byte hash, 4 byte length
-        message = connection.recv(76)
+        message = connection.recv(8)
 
         if message[:8] == COMM.encode():
-            # Unpack and return commitment and its hex
-            hash = message[8:72]
-            length = int.from_bytes(message[72:76], 'big')
+            # Recieve and unpack the hash
+            message = connection.recv(4)
+            hash_length = int.from_bytes(message, 'big')
+            hash = connection.recv(hash_length)
+
+            # Recieve and unpack the commitment
+            message = connection.recv(4)
+            length = int.from_bytes(message, 'big')
             message = connection.recv(length)
             commitment = message
             break
--- shurmann.py
+++ shurmann.py
@@ -1,4 +1,5 @@
 import time
+import math
 import multiprocessing as mp
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import constant_time
@@ -7,26 +8,30 @@ from reed_solomon import ReedSolomonObj
 from corrector import Fuzzy_Commitment
 from network import *
 
-# TODO: Make it so that key length is a parameter to the Shurmann and siggs protocol
-# It will need work with how many bits the quantizer
 # TODO: Make template for protocols so there is are guaranteed boiler plate functionality in how to initialize it
-# TODO: Make it so window_len and bands are parameterized
 class Shurmann_Siggs_Protocol:
-    def __init__(self, sensor, n, k, timeout, time_length, nfs_server_dir, identifier):
+    def __init__(self, sensor, key_length, parity_symbols, window_len, band_len, timeout, logger, verbose=True):
         self.sensor = sensor
-        self.re = Fuzzy_Commitment(ReedSolomonObj(n ,k), 8)
+        self.window_len = window_len
+        self.band_len = band_len
+
         self.name = "shurmann-siggs"
-        self.count = 0
         self.timeout = timeout
-        self.time_length = time_length
-        self.n = n
-        self.k = k
 
-        self.hash_func = hashes.SHA512() # Can't change this without breaking the commit network function (will fix later)
+        self.key_length = key_length
+        self.parity_symbols = parity_symbols
+        self.commitment_length = parity_symbols + key_length
+        self.re = Fuzzy_Commitment(ReedSolomonObj(self.commitment_length, key_length), key_length)
+        self.hash_func = hashes.SHA256()
 
-        # These variables should be in the NFS object
-        self.nfs_server_dir = nfs_server_dir
-        self.identifier = identifier
+        # Conversion from how many requested bits you need to how much sample data you will need for that
+        self.time_length = math.ceil(((self.commitment_length*8 ) / int((window_len/2 + 1) / band_len)) + 1)*window_len
+
+        self.logger = logger
+
+        self.count = 0
+
+        self.verbose = verbose
 
     def sigs_algo(self, x1, window_len=10000, bands=1000):
         def bitstring_to_bytes(s):
@@ -60,49 +65,61 @@ class Shurmann_Siggs_Protocol:
                 E[(i, j)] = np.sum(bands_lst[i][j])
 
         bs = ""
+
+        count = 0
         for i in range(1, len(FFTs)):
             for j in range(0, len(bands_lst[i]) - 1):
                 if E[(i, j)] - E[(i, j + 1)] - (E[(i - 1, j)] - E[(i - 1, j + 1)]) > 0:
                     bs += "1"
                 else:
                     bs += "0"
-
         return bitstring_to_bytes(bs)
 
     def extract_context(self):
-        signal = self.sensor.read(self.time_length * self.sensor.sample_rate) # TODO replace later
-        bits = self.sigs_algo(signal)
+        signal = self.sensor.read(self.time_length)
+        bits = self.sigs_algo(signal, window_len=self.window_len, bands=self.band_len)
         return bits, signal
 
     def device_protocol(self, host):
         host.setblocking(1)
-        print(f"Iteration {str(self.count)}.\n")
+        if self.verbose:
+            print(f"Iteration {str(self.count)}.\n")
 
         # Sending ack that they are ready to begin
-        print("Sending ACK.\n")
+        if self.verbose:
+            print("Sending ACK.\n")
         ack(host)
 
         # Wait for ack from host to begin context extract, quit early if no response within time
-        print("Waiting for ACK from host.")
+        if self.verbose:
+            print("Waiting for ACK from host.")
         if not ack_standby(host, self.timeout):
-            print("No ACK recieved within time limit - early exit.\n\n")
+            if self.verbose:
+                print("No ACK recieved within time limit - early exit.\n\n")
             return
 
         # Extract bits from mic
-        print("Extracting context\n")
+        if self.verbose:
+            print("Extracting context\n")
         witness, signal = self.extract_context()
 
+        if self.verbose:
+            print("witness: " + str(witness))
+
         # Wait for Commitment
-        print("Waiting for commitment from host")
+        if self.verbose:
+            print("Waiting for commitment from host")
         commitment, recieved_hash = commit_standby(host, self.timeout)
 
         # Early exist if no commitment recieved in time
         if not commitment:
-            print("No commitment recieved within time limit - early exit\n")
+            if self.verbose:
+                print("No commitment recieved within time limit - early exit\n")
             return
 
         # Decommit
-        print("Decommiting")
+        if self.verbose:
+            print("Decommiting")
         key = self.re.decommit_witness(commitment, witness)
 
         generated_hash = self.hash_function(key)
@@ -111,13 +128,15 @@ class Shurmann_Siggs_Protocol:
         if constant_time.bytes_eq(generated_hash, recieved_hash):
             success = True
 
-        print(f"key: {str(key)}\n success: {str(success)}\n")
+        if self.verbose:
+            print(f"key: {str(key)}\n success: {str(success)}\n")
 
         self.count += 1
 
     def host_protocol(self, device_sockets):
-        print("Iteration " + str(self.count))
-        print()
+        if self.verbose:
+            print("Iteration " + str(self.count))
+            print()
         for device in device_sockets:
             p = mp.Process(target=self.host_protocol_single_threaded, args=[device])
             p.start()
@@ -126,27 +145,36 @@ class Shurmann_Siggs_Protocol:
 
         # Exit early if no devices to pair with
         if not ack_standby(device_socket, self.timeout):
-            print("No ACK recieved within time limit - early exit.\n\n")
+            if self.verbose:
+                print("No ACK recieved within time limit - early exit.\n\n")
             return
-        print("Successfully ACKed participating device")
-        print()
+        if self.verbose:
+            print("Successfully ACKed participating device")
+            print()
 
-        print("ACKing all participating devices")
+        if self.verbose:
+            print("ACKing all participating devices")
         ack(device_socket)
 
         # Extract key from mic
-        print("Extracting Context")
+        if self.verbose:
+            print("Extracting Context\n")
         witness, signal = self.extract_context()
-        print()
 
         # Commit Secret
-        print("Commiting Witness")
+        if self.verbose:
+            print("Commiting Witness")
+
         secret_key, commitment = self.re.commit_witness(witness)
+
+        if self.verbose:
+            print("witness: " + str(witness))
 
         hash = self.hash_function(secret_key)
 
-        print("Sending commitment")
-        print()
+        if self.verbose:
+            print("Sending commitment")
+            print()
         send_commit(commitment, hash, device_socket)
 
         self.count += 1
@@ -157,7 +185,7 @@ class Shurmann_Siggs_Protocol:
         return hash_func.finalize()
 
 
-'''###TESTING CODE###
+###TESTING CODE###
 import socket
 def device(prot):
     print("device")
@@ -180,9 +208,17 @@ def host(prot):
 
 if __name__ == "__main__":
     import multiprocessing as mp
-    import random
-    prot = Shurmann_Siggs_Protocol(None, 12, 8, 10, None, None)
+    from test_sensor import Test_Sensor
+    from sensor_reader import Sensor_Reader
+    prot = Shurmann_Siggs_Protocol(Sensor_Reader(Test_Sensor(44100, 44100*400, 1024)),
+                                   8,
+                                   4,
+                                   10000,
+                                   1000,
+                                   10,
+                                   None,
+    )
     h = mp.Process(target=host, args=[prot])
     d = mp.Process(target=device, args=[prot])
     h.start()
-    d.start()'''
+    d.start()
