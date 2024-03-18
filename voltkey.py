d0eca9f6c23e1d6d65bb567d35b9373b9ece2a2b
Juan C Martinez <jmartinez29@luc.edu>
2024-03-18 17:02:55 -0500

    Added verbosity argument for VoltKey protocol.

--- voltkey.py
+++ voltkey.py
@@ -3,16 +3,18 @@ import serial
 
 from sensor_interface import SensorInterface
 
-SETUP      = "s".encode()
-ACCEPT     = "a".encode()
-CANCEL     = "x".encode()
-GO         = "g".encode()
-HARD_RESET = "h".encode()
-RESET      = "r".encode()
+SETUP = "s".encode()
+ACCEPT = "a".encode()
+CANCEL = "x".encode()
+GO = "g".encode()
+HALT = "h".encode()
+RESET = "r".encode()
+HELLO = "y".encode()
+EOL = "\r\n".encode()
 
 
 class Voltkey(SensorInterface):
-    def __init__(self, sample_rate, buffer_size, chunk_size):
+    def __init__(self, sample_rate, buffer_size, chunk_size, verbose=False):
         SensorInterface.__init__(self)
         self.sample_rate = sample_rate
         self.buffer_size = buffer_size
@@ -20,6 +22,7 @@ class Voltkey(SensorInterface):
         self.chunks = self.buffer_size // self.chunk_size
         self.name = "voltkey"
         self.buffer = np.zeros(chunk_size, np.float32())
+        self.verbose = verbose
         self.buffer_index = 0
         self.data_type = self.buffer.dtype
         self.sensor = serial.Serial("/dev/ttyS0")
@@ -27,23 +30,55 @@ class Voltkey(SensorInterface):
         self.start_thread()
 
     def start(self):
+        if self.verbose:
+            print(f"Starting up {self.name} sensor.\n")
+            print("Handshaking with sensor.\n")
+        self.sensor.write(HELLO)
+
+        if self.verbose:
+            print("Waiting for sensor to acknowledge.\n")
+        serial_message = self.sensor.read_until(EOL)
+
+        if serial_message != HELLO:
+            if self.verbose:
+                print("Handshake failed. Aborting.\n")
+
+            return
+
+        if self.verbose:
+            print(f"Sending chunk size of argument. Chunk size: {self.chunk_size}.\n")
         self.sensor.write(SETUP)
 
+        if self.verbose:
+            serial_message = self.sensor.read_until(EOL)
+            print(f"Recieved from sensor: {serial_message.decode()}.\n")
+
         for char in str(self.chunk_size):
-            self.sensor.writable(char.encode())
+            self.sensor.write(char.encode())
+
+            if self.verbose:
+                serial_message = self.sensor.read_until(EOL)
+                print(f"Recieved from sensor: {serial_message.decode()}.\n")
 
         self.sensor.write(ACCEPT)
 
+        if self.verbose:
+            serial_message = self.sensor.read_until(EOL)
+            print(f"Recieved from sensor: {serial_message.decode()}.\n")
+
     def stop(self):
+        if self.verbose:
+            print(f"Stopping {self.sensor.name} reading.\n")
+
         self.sensor.write(CANCEL)
 
-    def read(self):
-        # TODO Revisit this with Max
-        data = np.empty(self.chunk_size, self.data_type)
-        signal = self.sensor.read(self.chunk_size)
+        if self.verbose:
+            serial_message = self.sensor.read_until(EOL)
+            print(f"Recieved from sensor: {serial_message.decode()}.\n")
 
-        for i in range(self.chunk_size):
-            data[i] = signal[i]
+    def read(self):
+        signal = self.sensor.read(self.chunk_size)
+        data = np.array(signal, dtype=self.data_type)
 
         return data
 
