aec7ca1221c067907eda9f09192dbb6b1aa93406
isaac-ahlgren <isaacahlgren12@gmail.com>
2024-04-04 15:36:26 -0500

    small changes

--- nfs.py
+++ nfs.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import mysql.connector
 import numpy as np
 
-
 class NFSLogger:
     def __init__(self, user, password, host, database, nfs_server_dir, identifier, local_dir=None):
         self.user = user
@@ -15,9 +14,8 @@ class NFSLogger:
         self.identifier = identifier
         self.local_dir = local_dir
 
-    def log(self, data, count=None, ip_addr=None):
+    def log(self, data_tuples, count=None, ip_addr=None):
         # Timestamp only used in the creation of a file
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
         # Using indexing instead
         if self.local_dir:
@@ -28,12 +26,7 @@ class NFSLogger:
         for name, file_ext, data in data_tuples:
 
             # Create filename
-            filename = f"{directory}/{name}_id{self.identifier}_{timestamp}"
-            if count is not None:
-                filename += f"_count{count}"
-            if ip_addr is not None:
-                filename += f"_toipaddr{ip_addr}"
-            filename += f".{file_ext}"
+            filename = self.create_filename(directory, name, count, ip_addr, file_ext)
 
             # Determine mode based on whether data is bytes or str (it can be a list of bytes too)
             if isinstance(data, bytes) or (isinstance(data, list) and isinstance(data, bytes)):
@@ -59,10 +52,18 @@ class NFSLogger:
                         str(data)
                     )  # Data is a string, no encoding needed in text mode
 
-            
             # log file path to mysql database
             if not self.local_dir:
                 self._log_to_mysql([filename])
+
+    def create_filename(self, directory, name, count, ip_addr, file_ext):
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = f"{directory}/{name}_id{self.identifier}_{timestamp}"
+        if count is not None:
+            filename += f"_count{count}"
+        if ip_addr is not None:
+                filename += f"_toipaddr{ip_addr}"
+        filename += f".{file_ext}"
 
     def _log_to_mysql(self, file_paths):
         try:
