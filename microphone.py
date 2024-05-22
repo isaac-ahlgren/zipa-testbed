58798bdc299e1772554bf1d6caad3db2e4b8dec3
isaac-ahlgren <isaacahlgren12@gmail.com>
2024-05-22 15:44:11 -0500

    testing for rms calculation at 120 hz for mic

--- microphone.py
+++ microphone.py
@@ -10,23 +10,23 @@ import os
 import wave
 
 class Microphone(SensorInterface):
-    def __init__(self, sample_rate, buffer_size, chunk_size):
+    def __init__(self, sample_rate, buffer_size, chunk_size, mic_sampling_rate=48000):
         SensorInterface.__init__(self)
         self.format = pyaudio.paInt32  # Change to 16-bit format
         self.sample_rate = sample_rate
         self.name = "mic"
         self.pyaud = pyaudio.PyAudio()
-        self.buffer_size = buffer_size
         self.chunk_size = chunk_size
+        self.samples_per_spl_sample = mic_sampling_rate // sample_rate
+        self.buffer_size = buffer_size*self.samples_per_spl_sample
         self.stream = self.pyaud.open(
             format=self.format,
             channels=1,  # Stereo audio
-            rate=sample_rate,
+            rate=mic_sampling_rate,
             input=True,
-            frames_per_buffer=buffer_size,
+            frames_per_buffer=self.buffer_size
         )
         self.data_type = np.int32()
-        self.chunks_per_buffer = int(self.buffer_size / self.chunk_size)
         self.count = 0
         self.start_thread()
 
@@ -39,36 +39,25 @@ class Microphone(SensorInterface):
         self.buffer_ready.value = False
         self.stream.stop_stream()
 
+    def calc_rms(self, signal):
+        output = np.zeros(self.chunk_size, dtype=np.float32)
+        for i in range(self.chunk_size):
+            output[i] = np.sqrt(np.mean(signal[i*self.samples_per_spl_sample:(i+1)*self.samples_per_spl_sample]**2)) / len(signal)
+        rms = np.sqrt(np.mean(signal**2)) / len(signal)
+        return rms
+
     def read(self):
-        output = self.stream.read(self.chunk_size)
+        output = self.stream.read(self.samples_per_spl_sample*self.chunk_size)
         buf = np.frombuffer(
             output, dtype=np.int32
         )
-        return buf
+        return self.calc_rms(buf)
         
 
 # Test case
 if __name__ == "__main__":
     from sensor_reader import Sensor_Reader
-    import time
-    import matplotlib.pyplot as plt
-    mic = Microphone(44100, 44100*5, 1024) # Changed to 5; quicker
-    sr = Sensor_Reader(mic)
-    time.sleep(3)
-    print("getting ready to read")
-    results = sr.read(5*44100)
-    # plt.plot(sr.read(5*44100)) # Changed to 5; quicker
-    for i in range(5):
-        print(f"Iteration {i}")
-        results = sr.read(5 * 44100)
-        print(results)
-        time.sleep(5)
-    print("results collected, saving file") 
-    filename = "recording.wav"
-    with wave.open(filename, "wb") as wf:
-        wf.setnchannels(1)
-        wf.setsampwidth(mic.pyaud.get_sample_size(pyaudio.paInt32))
-        wf.setframerate(mic.sample_rate)
-        wf.writeframes(b"".join(results))
-    print(f"Audio file written. Size: {os.path.getsize(filename)} bytes")
-    plt.show()  
+  
+    mic = Microphone(120, 1000000, 120) 
+    sen_reader = Sensor_Reader(mic)
+    print(sen_reader.read(240))
