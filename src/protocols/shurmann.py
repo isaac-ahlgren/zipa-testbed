cdc6fd612e05aa3a55fb776e6ff6d62ee80b8ef9
Juan C Martinez <jmartinez29@luc.edu>
2024-06-24 16:04:58 -0500

    Schurmann function to prevent aliasing.

--- src/protocols/shurmann.py
+++ src/protocols/shurmann.py
@@ -95,6 +95,44 @@ class Shurmann_Siggs_Protocol:
                 else:
                     bs += "0"
         return bitstring_to_bytes(bs)
+    
+    def zero_out_antialias_sigs_algo(x1, antialias_freq, sampling_freq, window_len=10000, bands=1000):
+        FFTs = []
+        from scipy.fft import fft, fftfreq, ifft, rfft, irfft
+    
+        if window_len == 0:
+            window_len = len(x)
+    
+        freq_bin_len = (sampling_freq/2) / (int(window_len/2) + 1)
+        antialias_bin = int(antialias_freq / freq_bin_len)
+    
+        x = np.array(x1.copy())
+        #wind = scipy.signal.windows.hann(window_len)
+        for i in range(0,len(x),window_len):
+            if len(x[i:i+window_len]) < window_len:
+                #wind = scipy.signal.windows.hann(len(x[i:i+window_len]))
+                x[i:i+window_len] = x[i:i+window_len] #* wind
+            else:
+                x[i:i+window_len] = x[i:i+window_len] #* wind
+    
+            fft_row = abs(rfft(x[i:i+window_len]))
+            FFTs.append(fft_row[:antialias_bin])
+        E = {}
+        bands_lst = []
+        for i in range(0,len(FFTs)):
+            frame = FFTs[i]
+            bands_lst.append([ frame[k:k+bands] for k in range(0,len(frame),bands)])
+            for j in range(0,len(bands_lst[i])):
+                E[(i,j)] = np.sum(bands_lst[i][j])
+    
+        bs = ""
+        for i in range(1,len(FFTs)):
+            for j in range(0,len(bands_lst[i])-1):
+                if (E[(i,j)] -E[(i,j+1)]) - (E[(i-1,j)] - E[(i-1,j+1)]) > 0:
+                    bs+= "1"
+                else:
+                    bs+="0"   
+        return bs
 
     def extract_context(self):
         signal = self.sensor.read(self.time_length)
