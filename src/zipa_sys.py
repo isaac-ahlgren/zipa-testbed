8935f62445a3ec74f14299455a8829c80a7b306d
Juan C Martinez <jmartinez29@luc.edu>
2024-07-01 15:30:17 -0500

    Removing debugging statements; automated protocol instantiation works.

--- src/zipa_sys.py
+++ src/zipa_sys.py
@@ -210,26 +210,22 @@ class ZIPA_System:
         return participants
 
     def create_protocol(self, payload):
-        print(f"In create protocol function.\n")
         requested_name = payload["name"]
         sensor = payload["parameters"]["sensor"]
 
         if sensor not in self.sensors:
             raise Exception("Sensor not supported")
 
-        print(f"Iterating thorugh possible protocols.\n")
         for _, module_name, _ in pkgutil.iter_modules(protocols.__path__):
             module = __import__(f"protocols.{module_name}", fromlist=module_name)
 
             for name, obj in inspect.getmembers(module):
-                print(f"Currently inspecting {name}.\n")
                 if (
                     inspect.isclass(obj)
                     and issubclass(obj, ProtocolInterface)
                     and obj is not ProtocolInterface
                     and name == requested_name
                 ):
-                    print(f"Protocol found and adding to active sensor.")
                     self.protocols.append(
                         obj(payload["parameters"], self.sensors[sensor], self.logger)
                     )
