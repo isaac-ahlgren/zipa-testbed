59c4a66320b4d21b4d3454984f7135fe0e1ea179
rue <rhu1@luc.edu>
2024-07-05 11:03:25 -0500

    optimizing script because snr calculations take forever to run on a larger scale

--- eval/rolling_rms_snr.py
+++ eval/rolling_rms_snr.py
@@ -1,38 +1,45 @@
 import numpy as np
-from matplotlib import pyplot as plt 
-import statistics as st
+from matplotlib import pyplot as plt
 
-file = 'mic_id_10.0.0.227_date_2024062006.csv'
+device = 'mic'
+id_num = '231'
+hr = '06'
 
-data = np.array([])
+file = f'{device}_id_10.0.0.{id_num}_date_20240620{hr}.csv'
+
 data = np.genfromtxt(file, delimiter=',')
 
-window_size = 20000
-snr_window_size = 20000
+window_size = 240000
+snr_window_size = 240000
 
-rms_data = np.zeros(len(data) - window_size + 1)
-snr_data = np.zeros(len(rms_data) - snr_window_size + 1)
+def rolling_rms(data, window_size):
+    cumsum = np.cumsum(np.insert(data**2, 0, 0))
+    window_sums = cumsum[window_size:] - cumsum[:-window_size]
+    return np.sqrt(window_sums / window_size)
 
-# rolling RMS code 
-for i in range(len(data) - window_size + 1):
-    window = data[i:i + window_size]
-    rms = np.sqrt(np.mean(window**2))
+rms_data = rolling_rms(data, window_size)
 
-    rms_data[i] = rms
+# Calculate rolling mean and standard deviation for SNR calculation
+def rolling_mean_std(data, window_size):
+    cumsum = np.cumsum(np.insert(data, 0, 0))
+    cumsum_sq = np.cumsum(np.insert(data**2, 0, 0))
+    
+    sum_ = cumsum[window_size:] - cumsum[:-window_size]
+    sum_sq = cumsum_sq[window_size:] - cumsum_sq[:-window_size]
+    
+    mean = sum_ / window_size
+    std = np.sqrt(sum_sq / window_size - (mean**2))
+    
+    return mean, std
 
+mean, std = rolling_mean_std(rms_data, snr_window_size)
 
-# rolling snr code in respect to whatever you make it respect ig 
-for j in range(len(rms_data) - snr_window_size + 1):
-    snr_window = rms_data[j:j + snr_window_size]
-    mean = st.mean(snr_window)
-    std = st.stdev(snr_window)
-    std_squared = np.where(std**2 == 0, np.finfo(float).eps, std**2)
-    snr = 10 * np.log10((mean * 2) / std_squared)
+std_squared = np.where(std**2 == 0, np.finfo(float).eps, std**2)
+snr_data = 10 * np.log10((mean * 2) / std_squared)
 
-    snr_data[j] = snr
-
-
-plt.plot(snr_data)
-plt.plot(rms_data)
-plt.savefig('snr_data/naming_convention.png')
-plt.show()
+plt.plot(snr_data, label='SNR Data')
+plt.plot(rms_data[:len(snr_data)], label='RMS Data')
+plt.legend()
+plt.title('SNR and RMS Data')
+plt.savefig(f'snr_data/{device}_{id}_{hr}.png)
+# plt.show()
