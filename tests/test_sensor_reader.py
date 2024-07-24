88a8fda7010435ad43c645b5fa2415e5df2651fe
rue <rhu1@luc.edu>
2024-07-24 16:50:34 -0500

    Committing final stages of sensor_reader tests for pi testing

--- tests/test_sensor_reader.py
+++ tests/test_sensor_reader.py
@@ -40,11 +40,48 @@ def test_add_protocol():
     )
     test_sen_reader = SensorReader(test_sensor)
 
+    # Make queue tuple here
+    flag = Value("i", 0)
+    queue = Queue()
+    queue_size = len(test_sen_reader.queues)
+
+    status_queue = (flag, queue)
+
+    # read from queue here (it'll hopefully be being filled by the sensor reader)
+    test_sen_reader.add_protocol_queue(status_queue)
+
+    flag.value = 1
+    time.sleep(5)
+    flag.value = -1
+
+    test_sen_reader.queues.append(status_queue)
+
+    # checks to see if the status_queue is being written to 
+    assert status_queue[1].qsize() > 0
+
+    # checks to see if tuples are being added to the queue
+    assert len(test_sen_reader.queues) > queue_size
+    
 def test_remove_protocol_queue():
     test_sensor = TestSensor(
       SEN_TEST, signal_type="sine"
     )
     test_sen_reader = SensorReader(test_sensor)
+
+    flag = Value("i", 0)
+    queue = Queue()
+    queue_size = len(test_sen_reader.queues)
+
+    protocol_queue = (flag, queue)
+
+    # add tuple to queues 
+    test_add_protocol()
+
+    # call remove_protocol_queue 
+    test_sen_reader.remove_protocol_queue(protocol_queue)
+
+    # check to see if queues is empty
+    assert len(test_sen_reader.queues) == 0
 
 def test_poll():
     # raw data
@@ -64,14 +101,11 @@ def test_poll():
 
     # read from queue here (it'll hopefully be being filled by the sensor reader)
     test_sen_reader.add_protocol_queue(status_queue)
+
     flag.value = 1
     time.sleep(5)
     flag.value = -1
-    
-    
-    print(f'poll array: {test_sensor.read()}')
-    # Compare output of test_sensor.output_data and data gotten from queue
-    # assert status_queue == test_sensor.read
-    assert status_queue[1].qsize() > 0
 
-
+    # checks to see if the status_queue is being written to 
+    assert status_queue[1] == test_sensor.read()
+    
