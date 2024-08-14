947380c18d0cb71c77035f7fcc82bf05e48c35f3
MadeCbt <jamodemaja12@gmail.com>
2024-08-14 17:59:15 -0500

    commit_standby function return statements were flipped. committing for revision review @jc-mart

--- tests/test_network2.py
+++ tests/test_network2.py
@@ -81,9 +81,16 @@ def test_send_commit():
         device_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         device_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         device_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        device_socket.connect(("127.0.0.1", 2002))
-        send_commit([b'commitment', b'commitment'], [b'hash1', b'hash2'], device_socket)
+        device_socket.connect(("127.0.0.1", 2050))
+
+        commitments_list = [b'\x9f\x83\x5d\x7d\x3c\x23\x44\x55', b'\x1b\x9a\x2d\xef\x38\x4f\x63\x9c', b'\x4a\x62\x7d\xd3\x54\xc9\x8b\x03']
+        hashes_list = [b'\x6a\x8f\x44\xee\x21\x78\x3b\x97', b'\x1d\x9b\x7c\x5a\x2e\xaf\x76\x84', b'\x38\x5e\x16\x2a\x68\x7c\x9d\x51']
+
+        send_commit(commitments_list, hashes_list, device_socket)
         device_socket.close()
+
+    commitments_list = [b'\x9f\x83\x5d\x7d\x3c\x23\x44\x55', b'\x1b\x9a\x2d\xef\x38\x4f\x63\x9c', b'\x4a\x62\x7d\xd3\x54\xc9\x8b\x03']
+    hashes_list = [b'\x6a\x8f\x44\xee\x21\x78\x3b\x97', b'\x1d\x9b\x7c\x5a\x2e\xaf\x76\x84', b'\x38\x5e\x16\x2a\x68\x7c\x9d\x51']
 
     device_process = Process(target=device, name="[CLIENT]")
     device_process.start()
@@ -91,13 +98,19 @@ def test_send_commit():
     host_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     host_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    host_socket.bind(("127.0.0.1", 2002))
+    host_socket.bind(("127.0.0.1", 2050))
     host_socket.listen(1)
     connection, _ = host_socket.accept()
 
     commitments, hashes = commit_standby(connection, 5)
-    assert commitments == [b'commitment', b'commitment']
-    assert hashes == [b'hash', b'hash']
+
+    # print(f'{type(commitments_list)}: {commitments_list}')
+    # print(f'{type(commitments)}: {commitments}')
+    # print(f'{type(hashes)}: {hashes}')
+    # print(f'{type(hashes_list)}: {hashes_list}')
+
+    assert commitments == commitments_list
+    assert hashes == hashes_list
 
     connection.close()
     host_socket.close()
