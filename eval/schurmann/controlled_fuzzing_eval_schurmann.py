d44140479c05a610d6a9cafea03d63ebcf63aa7b
isaac-ahlgren <isaacahlgren12@gmail.com>
2024-09-04 19:49:28 -0500

    checkpoint

--- eval/schurmann/controlled_fuzzing_eval_schurmann.py
+++ eval/schurmann/controlled_fuzzing_eval_schurmann.py
@@ -15,18 +15,18 @@ from schurmann_tools import (
 
 sys.path.insert(1, os.getcwd() + "/..")  # Gives us path to eval_tools.py
 from eval_tools import (  # noqa: E402
-    cmp_bits,
     load_controlled_signal_files,
     log_bytes,
     log_parameters,
     gen_id
 )
 from evaluator import Evaluator  # noqa: E402
+from signal_file import Signal_File # noqa: E402
 
 # Static default parameters
 KEY_LENGTH_DEFAULT = 128
 NUMBER_OF_KEYS = 100
-NUMBER_OF_CHOICES_DEFAULT = 1000
+NUMBER_OF_CHOICES_DEFAULT = 100
 
 # Random Parameter Ranges
 WINDOW_LENGTH_RANGE = (5000, 2*48000)
@@ -36,30 +36,31 @@ def main(
     key_length=KEY_LENGTH_DEFAULT,
     number_of_choices=NUMBER_OF_CHOICES_DEFAULT,
 ):
-    if os.path.isdir(DATA_DIRECTORY):
+    if not os.path.isdir(DATA_DIRECTORY):
         os.mkdir(DATA_DIRECTORY)
 
     signals = load_controlled_signal_files()
 
     def get_random_parameters():
         window_length = random.randint(WINDOW_LENGTH_RANGE[0], WINDOW_LENGTH_RANGE[1])
-        band_length = random.randint(MIN_BAND_LENGTH, (WINDOW_LENGTH_RANGE[1] // 2 + 1) // 2)
+        band_length = random.randint(MIN_BAND_LENGTH, (window_length // 2 + 1) // 2)
         # Calculating the number of samples needed
+        print(f"window_length: {window_length}, band_length: {band_length}")
         sample_num = schurmann_calc_sample_num(
             key_length, window_length, band_length, MICROPHONE_SAMPLING_RATE, ANTIALIASING_FILTER
         )
+        print(f"sample_num: {sample_num}")
         return window_length, band_length, MICROPHONE_SAMPLING_RATE, ANTIALIASING_FILTER, sample_num
 
-    def log(byte_list, *params):
-        id = gen_id()
-        os.mkdir(f"./{DATA_DIRECTORY}/schurmann_id{id}")
-        file_stub = f"{DATA_DIRECTORY}/schurmann_id{id}/schurmann_id{id}"
+    def log(byte_list, choice_id, signal_id, *params):
+        os.mkdir(f"./{DATA_DIRECTORY}/schurmann_id{choice_id}")
+        file_stub = f"{DATA_DIRECTORY}/schurmann_id{choice_id}/schurmann_id{choice_id}_{signal_id}"
         names = ["window_length", "band_length", "sample_num"]
         param_list = [params[0], params[1], params[4]]
         log_parameters(file_stub, names, param_list)
         log_bytes(file_stub, byte_list, key_length)
 
-    def bit_gen_algo(signal: Signal_Buffer, *argv: List) -> np.ndarray:
+    def bit_gen_algo(signal: Signal_File, *argv: List) -> np.ndarray:
         """
         Processes the signal using the Schurmann wrapper function to generate cryptographic bits.
 
@@ -75,7 +76,7 @@ def main(
     
     # Creating an evaluator object with the bit generation algorithm
     evaluator = Evaluator(bit_gen_algo, random_parameter_func=get_random_parameters, logging_func=log, event_driven=False)
-    evaluator.fuzzing_evaluation(signals)
+    evaluator.fuzzing_evaluation(signals, number_of_choices, multithreaded=False)
 
 if __name__ == "__main__":
     main()
