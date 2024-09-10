c39f2656f322a1c021fe3c831c021b10b69fb499
isaac-ahlgren <isaacahlgren12@gmail.com>
2024-09-10 00:05:39 -0500

    added some stuff so I can regenerate data and plot easier tomorrow

--- eval/plot_scripts/plot_controlled_fuzzing.py
+++ eval/plot_scripts/plot_controlled_fuzzing.py
@@ -7,7 +7,14 @@ def plot_schurmann():
     SCHURMANN_DATA_DIRECTORY = "../schurmann/schurmann_data"
     SCHURMANN_CONTROLLED_FUZZING_STUB = "schurmann_controlled_fuzz"
     contents = parse_eval_directory(SCHURMANN_DATA_DIRECTORY, SCHURMANN_CONTROLLED_FUZZING_STUB)
-    print(contents)
+
+    # Regenerate 500 examples for SNR 40, 30, 20, 10, 5
+
+    # Plot bit err vs parameter pick for window length
+
+    # Plot bit err vs parameter pick for band length
+
+    # Plot bar graph of correlation between bit err and window length and bit err and band length
 
 def main():
     plot_schurmann()
--- eval/schurmann/controlled_fuzzing_eval_schurmann.py
+++ eval/schurmann/controlled_fuzzing_eval_schurmann.py
@@ -1,6 +1,7 @@
 import os
 import random
 import sys
+import argparse
 from typing import List
 
 import numpy as np
@@ -15,7 +16,6 @@ from schurmann_tools import (
 sys.path.insert(1, os.getcwd() + "/..")  # Gives us path to eval_tools.py
 from eval_tools import (  # noqa: E402
     load_controlled_signal_files,
-    log_bytes,
     log_parameters,
 )
 from evaluator import Evaluator  # noqa: E402
@@ -31,8 +31,39 @@ WRAP_AROUND_LIMIT_DEFAULT = 10
 WINDOW_LENGTH_RANGE = (5000, 2 * 48000)
 MIN_BAND_LENGTH = 1
 
-SCHURMANN_CONTROLLED_FUZZING = "/schurmann_controlled_fuzz"
+SCHURMANN_CONTROLLED_FUZZING = "schurmann_controlled_fuzz"
 SCHURMANN_CONTROLLED_FUZZING_STUB = "schurmann_controlled_fuzz"
+
+def get_command_line_args(
+    key_length_default: int = KEY_LENGTH_DEFAULT,
+    target_snr_default: int = TARGET_SNR_DEFAULT,
+    number_of_choices_default: int = NUMBER_OF_CHOICES_DEFAULT,
+    wrap_around_limit_default: float = WRAP_AROUND_LIMIT_DEFAULT,
+):
+    """
+    Parse command-line arguments for the script.
+
+    :return: Tuple containing window length, band length, key length, SNR level, and number of trials.
+    """
+    parser = argparse.ArgumentParser()
+
+    # Add arguments without descriptions
+    parser.add_argument("-kl", "--key_length", type=int, default=key_length_default)
+    parser.add_argument("-snr", "--snr_level", type=float, default=target_snr_default)
+    parser.add_argument("-c", "--choices", type=int, default=number_of_choices_default)
+    parser.add_argument("-wwl", "--wrap_around_limit", type=int, default=wrap_around_limit_default)
+
+    # Parsing command-line arguments
+    args = parser.parse_args()
+
+    # Extracting arguments
+    key_length = getattr(args, "key_length")
+    target_snr = getattr(args, "snr_level")
+    number_of_choices = getattr(args, "choices")
+    wrap_around_limit = getattr(args, "wrap_around_limit")
+
+    return key_length, target_snr, number_of_choices, wrap_around_limit
+
 
 def main(
     key_length=KEY_LENGTH_DEFAULT,
@@ -43,8 +74,13 @@ def main(
     if not os.path.isdir(DATA_DIRECTORY):
         os.mkdir(DATA_DIRECTORY)
 
-    if not os.path.isdir(DATA_DIRECTORY + SCHURMANN_CONTROLLED_FUZZING):
-        os.mkdir(DATA_DIRECTORY + SCHURMANN_CONTROLLED_FUZZING)
+    if not os.path.isdir(f"{DATA_DIRECTORY}/{SCHURMANN_CONTROLLED_FUZZING}_snr{target_snr}"):
+        os.mkdir(f"{DATA_DIRECTORY}/{SCHURMANN_CONTROLLED_FUZZING}")
+
+    if not os.path.isdir(f"{DATA_DIRECTORY}/{SCHURMANN_CONTROLLED_FUZZING}/{SCHURMANN_CONTROLLED_FUZZING}_snr{target_snr}"):
+        os.mkdir(f"{DATA_DIRECTORY}/{SCHURMANN_CONTROLLED_FUZZING}/{SCHURMANN_CONTROLLED_FUZZING}_snr{target_snr}")
+
+    fuzzing_dir = f"{DATA_DIRECTORY}/{SCHURMANN_CONTROLLED_FUZZING}/{SCHURMANN_CONTROLLED_FUZZING}_snr{target_snr}"
 
     signals = load_controlled_signal_files(target_snr, wrap_around=True, wrap_around_limit=wrap_around_limit)
 
@@ -102,8 +138,9 @@ def main(
         parameter_log_func=log,
         event_driven=False,
     )
-    evaluator.fuzzing_evaluation(signals, number_of_choices, key_length, DATA_DIRECTORY + SCHURMANN_CONTROLLED_FUZZING, SCHURMANN_CONTROLLED_FUZZING_STUB, multithreaded=False)
+    evaluator.fuzzing_evaluation(signals, number_of_choices, key_length, fuzzing_dir, f"{SCHURMANN_CONTROLLED_FUZZING_STUB}_snr{target_snr}", multithreaded=False)
 
 
 if __name__ == "__main__":
-    main()
+    args = get_command_line_args()
+    main(*args)
