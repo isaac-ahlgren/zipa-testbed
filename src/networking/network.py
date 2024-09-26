eabe169dd8e477c23fa2025e2749a714b445502e
MadeCbt <jamodemaja12@gmail.com>
2024-09-26 14:17:15 -0500

    roll back

--- src/networking/network.py
+++ src/networking/network.py
@@ -1,4 +1,3 @@
-#clean netowrk
 import socket
 import time
 from typing import List, Optional, Tuple  # Union
@@ -15,6 +14,57 @@ SUCC = "success "
 FAIL = "failed  "
 FPFM = "fpake   "
 
+
+def send_status(connection: socket.socket, status: bool) -> None:
+    """
+    Sends a success or failure status over a connection.
+
+    :param connection: The network connection over which to send the status.
+    :param status: The boolean status indicating success (True) or failure (False).
+    """
+    if status:
+        msg = SUCC
+    else:
+        msg = FAIL
+    connection.send(msg.encode())
+
+
+def status_standby(connection: socket.socket, timeout: int) -> Optional[bool]:
+    """
+    Waits for a success or failure status message within a specified timeout period.
+
+    :param connection: The network connection to receive from.
+    :param timeout: The maximum time in seconds to wait for a status.
+    :returns: True if success, False if failure, None if timeout.
+    """
+    status = None
+    reference = time.time()
+
+    # Set the socket timeout
+    connection.settimeout(timeout)
+
+    try:
+        # While process hasn't timed out
+        while (time.time() - reference) < timeout:
+            # Attempt to receive the command
+            command = connection.recv(8)
+
+            if not command:  # Handle empty or closed connection
+                continue
+
+            if command == SUCC.encode():  # Check if the command is a success message
+                status = True
+                break
+            elif command == FAIL.encode():  # Check if the command is a failure message
+                status = False
+                break
+    except socket.timeout:
+        print("status not received within the timeout period.")
+        return None
+
+    return status
+
+
 def ack(connection: socket.socket) -> None:
     """
     Sends an acknowledgement message over a connection.
@@ -23,7 +73,8 @@ def ack(connection: socket.socket) -> None:
     """
     connection.send(ACKN.encode())
 
-def ack_standby(connection: socket.socket, timeout: int) -> bool:
+
+def ack_standby2(connection: socket.socket, timeout: int) -> bool:
     """
     Waits for an acknowledgement within a specified timeout period.
 
@@ -32,18 +83,55 @@ def ack_standby(connection: socket.socket, timeout: int) -> bool:
     :returns: True if acknowledged, False if timeout occurred.
     """
     acknowledged = False
-    connection.settimeout(timeout)
+    reference = time.time()
+    timestamp = reference
 
-    try:
+    # While process hasn't timed out
+    while (timestamp - reference) < timeout:
+        # Check for acknowledgement
+        timestamp = time.time()
         command = connection.recv(8)
-    except socket.timeout:
-        print("Ack not received within the timeout period.")
-        command = None
 
-    if command == ACKN.encode():
-        acknowledged = True
+        if command is None:
+            continue
+        elif command == ACKN.encode():
+            acknowledged = True
+            break
 
     return acknowledged
+
+
+def send_commit(
+    commitments: List[bytes], hashes: List[bytes], device: socket.socket
+) -> None:
+    """
+    Sends commitments along with their corresponding hashes over a network connection.
+
+    :param commitments: List of commitments to be sent.
+    :param hashes: List of hashes corresponding to the commitments.
+    :param device: The network connection to send data over.
+    """
+    # Prepare number of commitments and their lengths
+    number_of_commitments = len(commitments).to_bytes(4, byteorder="big")
+    com_length = len(commitments[0]).to_bytes(4, byteorder="big")
+
+    if hashes is not None:
+        hash_length = len(hashes[0])
+    else:
+        hash_length = 0
+
+    # Prepare hash length and begin packing payload
+    hash_length = hash_length.to_bytes(4, byteorder="big")
+    message = COMM.encode() + number_of_commitments + hash_length + com_length
+
+    for i in range(len(commitments)):
+        message += (
+            commitments[i] + hashes[i]
+        )  # Works in Shurmann since it has hashes to send
+        # message += commitments[i] # Works in mietinnen since it doens't do any hashes
+
+    device.send(message)
+
 
 def commit_standby(
     connection: socket.socket, timeout: int
@@ -117,6 +205,7 @@ def commit_standby(
 
     return commitments, hashes
 
+
 def dh_exchange(connection: socket.socket, key: bytes) -> None:
     """
     Sends a Diffie-Hellman key over a network connection.
@@ -127,6 +216,7 @@ def dh_exchange(connection: socket.socket, key: bytes) -> None:
     key_size = len(key).to_bytes(4, byteorder="big")
     message = DHKY.encode() + key_size + key
     connection.send(message)
+
 
 def dh_exchange_standby(connection: socket.socket, timeout: int) -> Optional[bytes]:
     """
@@ -164,6 +254,48 @@ def dh_exchange_standby(connection: socket.socket, timeout: int) -> Optional[byt
         return None  # Return None if timeout occurs
 
     return key
+
+
+def send_nonce_msg(connection: socket.socket, nonce: bytes) -> None:
+    """
+    Sends a nonce message over a network connection.
+
+    :param connection: The network connection to send the nonce over.
+    :param nonce: The nonce to send.
+    """
+    nonce_size = len(nonce).to_bytes(4, byteorder="big")
+    message = NONC.encode() + nonce_size + nonce
+    connection.send(message)
+
+
+def get_nonce_msg_standby2(connection: socket.socket, timeout: int) -> Optional[bytes]:
+    """
+    Waits to receive a nonce within a specified timeout period.
+
+    :param connection: The network connection to receive from.
+    :param timeout: The maximum time in seconds to wait for the nonce.
+    :returns: The received nonce if successful, None otherwise.
+    """
+    reference = time.time()
+    timestamp = reference
+    nonce = None
+
+    # While process hasn't timed out
+    while (timestamp - reference) < timeout:
+        timestamp = time.time()
+        message = connection.recv(12)
+
+        if message is None:
+            continue
+        else:
+            command = message[:8]
+            if command == NONC.encode():
+                nonce_size = int.from_bytes(message[8:], "big")
+                nonce = connection.recv(nonce_size)
+                break
+
+    return nonce
+
 
 def get_nonce_msg_standby(connection: socket.socket, timeout: int) -> Optional[bytes]:
     """
@@ -204,6 +336,46 @@ def get_nonce_msg_standby(connection: socket.socket, timeout: int) -> Optional[b
 
     return nonce
 
+
+def ack_standby(connection: socket.socket, timeout: int) -> bool:
+    """
+    Waits for an acknowledgement within a specified timeout period.
+
+    :param connection: The network connection to receive from.
+    :param timeout: The maximum time in seconds to wait for an acknowledgement.
+    :returns: True if acknowledged, False if timeout occurred.
+    """
+    acknowledged = False
+    connection.settimeout(timeout)
+
+    try:
+        command = connection.recv(8)
+    except socket.timeout:
+        print("Ack not received within the timeout period.")
+        command = None
+
+    if command == ACKN.encode():
+        acknowledged = True
+
+    return acknowledged
+
+
+def send_pake_msg(connection, msg):
+    length_payload = 0
+    for m in msg:
+        length_payload += len(m) + 4
+
+    payload = length_payload.to_bytes(4, byteorder="big")
+
+    for m in msg:
+        payload += len(m).to_bytes(4, byteorder="big")
+        payload += m
+
+    outgoing = FPFM.encode() + payload
+
+    connection.send(outgoing)
+
+
 def pake_msg_standby(connection: socket.socket, timeout: int) -> bool:
 
     msg = None
@@ -238,109 +410,3 @@ def pake_msg_standby(connection: socket.socket, timeout: int) -> bool:
         print("fpake not received within the timeout period.")
 
     return msg
-
-def send_commit(
-    commitments: List[bytes], hashes: List[bytes], device: socket.socket
-) -> None:
-    """
-    Sends commitments along with their corresponding hashes over a network connection.
-
-    :param commitments: List of commitments to be sent.
-    :param hashes: List of hashes corresponding to the commitments.
-    :param device: The network connection to send data over.
-    """
-    # Prepare number of commitments and their lengths
-    number_of_commitments = len(commitments).to_bytes(4, byteorder="big")
-    com_length = len(commitments[0]).to_bytes(4, byteorder="big")
-
-    if hashes is not None:
-        hash_length = len(hashes[0])
-    else:
-        hash_length = 0
-
-    # Prepare hash length and begin packing payload
-    hash_length = hash_length.to_bytes(4, byteorder="big")
-    message = COMM.encode() + number_of_commitments + hash_length + com_length
-
-    for i in range(len(commitments)):
-        message += (
-            commitments[i] + hashes[i]
-        )  # Works in Shurmann since it has hashes to send
-        # message += commitments[i] # Works in mietinnen since it doens't do any hashes
-
-    device.send(message)
-
-def send_nonce_msg(connection: socket.socket, nonce: bytes) -> None:
-    """
-    Sends a nonce message over a network connection.
-
-    :param connection: The network connection to send the nonce over.
-    :param nonce: The nonce to send.
-    """
-    nonce_size = len(nonce).to_bytes(4, byteorder="big")
-    message = NONC.encode() + nonce_size + nonce
-    connection.send(message)
-
-def send_pake_msg(connection, msg):
-    length_payload = 0
-    for m in msg:
-        length_payload += len(m) + 4
-
-    payload = length_payload.to_bytes(4, byteorder="big")
-
-    for m in msg:
-        payload += len(m).to_bytes(4, byteorder="big")
-        payload += m
-
-    outgoing = FPFM.encode() + payload
-
-    connection.send(outgoing)
-
-def send_status(connection: socket.socket, status: bool) -> None:
-    """
-    Sends a success or failure status over a connection.
-
-    :param connection: The network connection over which to send the status.
-    :param status: The boolean status indicating success (True) or failure (False).
-    """
-    if status:
-        msg = SUCC
-    else:
-        msg = FAIL
-    connection.send(msg.encode())
-
-def status_standby(connection: socket.socket, timeout: int) -> Optional[bool]:
-    """
-    Waits for a success or failure status message within a specified timeout period.
-
-    :param connection: The network connection to receive from.
-    :param timeout: The maximum time in seconds to wait for a status.
-    :returns: True if success, False if failure, None if timeout.
-    """
-    status = None
-    reference = time.time()
-
-    # Set the socket timeout
-    connection.settimeout(timeout)
-
-    try:
-        # While process hasn't timed out
-        while (time.time() - reference) < timeout:
-            # Attempt to receive the command
-            command = connection.recv(8)
-
-            if not command:  # Handle empty or closed connection
-                continue
-
-            if command == SUCC.encode():  # Check if the command is a success message
-                status = True
-                break
-            elif command == FAIL.encode():  # Check if the command is a failure message
-                status = False
-                break
-    except socket.timeout:
-        print("status not received within the timeout period.")
-        return None
-
-    return status
-
