b47464e209b084505d37e1f738d5cfb2a303d03c
MadeCbt <jamodemaja12@gmail.com>
2024-10-03 17:36:43 -0500

    starting

---
+++ eval/plot_scripts/plot_data_week.py
@@ -0,0 +1,8 @@
+import glob 
+import os 
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+sys.path.insert(1, os.getcwd() + "/..") # give us path to eval/plot_scripts
