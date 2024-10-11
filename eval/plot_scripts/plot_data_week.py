09a80c08b677d89f3364c412a2eee5bf56329675
LukeNiemeyer <ltniemo@gmail.com>
2024-10-11 00:03:14 -0500

    Plots created

--- eval/plot_scripts/plot_data_week.py
+++ eval/plot_scripts/plot_data_week.py
@@ -13,6 +13,7 @@ sys.path.insert(1, os.path.join(os.getcwd(), "eval"))
 
 
 from eval_tools import load_parameters  # noqa: E402
+from utils import load_bytes
 
 
 # Set PLOTTING_DIR to the correct path within ZIPA-TESTBED
@@ -28,25 +29,6 @@ if not os.path.exists(PLOTTING_DIR):
 DEVICE_PAIRS = [
    ("10.0.0.227", "10.0.0.229", "10.0.0.237")  # Only using 3 devices
 ]
-
-
-
-
-def load_bytes(byte_file):
-   """
-   Loads bits from a file and returns them as a list of strings.
-   Adds error handling in case the file is not found.
-   """
-   try:
-       with open(byte_file, "r") as file:
-           output = file.readlines()
-       output = [bits.strip() for bits in output]
-       return output
-   except FileNotFoundError:
-       print(f"File {byte_file} not found.")
-       return []
-
-
 
 
 def get_block_err(bits1, bits2, block_size):
@@ -185,9 +167,9 @@ def save_to_csv(legit_comparison, adv_comparison, custom_csv_filename):
 if __name__ == "__main__":
    # Loading the new bit files into the week_data list
    week_data = [
-       load_bytes("local_data/schurmann_real_full/schurmann_real_eval_full_two_weeks_10.0.0.227_bits.txt"),
-       load_bytes("local_data/schurmann_real_full/schurmann_real_eval_full_two_weeks_10.0.0.229_bits.txt"),
-       load_bytes("local_data/schurmann_real_full/schurmann_real_eval_full_two_weeks_10.0.0.237_bits.txt")
+       load_bytes("local_data/schurmann_real_full/schurmann_real_eval_full_two_weeks_10.0.0.238_10.0.0.228_10.0.0.239/schurmann_real_eval_full_two_weeks_10.0.0.238_bits.txt"),
+       load_bytes("local_data/schurmann_real_full/schurmann_real_eval_full_two_weeks_10.0.0.238_10.0.0.228_10.0.0.239/schurmann_real_eval_full_two_weeks_10.0.0.228_bits.txt"),
+       load_bytes("local_data/schurmann_real_full/schurmann_real_eval_full_two_weeks_10.0.0.238_10.0.0.228_10.0.0.239/schurmann_real_eval_full_two_weeks_10.0.0.239_bits.txt")
    ]
 
 
@@ -196,8 +178,8 @@ if __name__ == "__main__":
 
 
    # Plot combined comparisons for hourly data over the week with a custom plot filename
-   plot_ber_comparisons(legit_comparison, adv_comparison, custom_plot_filename="custom_ber_comparison_plot_week.pdf")
+   plot_ber_comparisons(legit_comparison, adv_comparison, custom_plot_filename="238-228-239_ber_comparison_plot_week.pdf")
 
 
    # Save the BER data to a custom CSV file
-   save_to_csv(legit_comparison, adv_comparison, custom_csv_filename="custom_ber_comparison_data_week.csv")
+   save_to_csv(legit_comparison, adv_comparison, custom_csv_filename="238-228-239_ber_comparison_data_week.csv")
