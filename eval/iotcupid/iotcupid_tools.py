fixed iotcupid_tools.py to preprocess event signals into its derivatives

eval/iotcupid/iotcupid_tools.py
@@ -39,9 +39,28 @@ def adversary_signal(sample_num):
     """
     return adv_rng.integers(0, 10, size=sample_num)
 
-def process_events(events, event_signals, key_size, feature_dim, 
+def preprocess_event_signals(event_signals, window_size, a):
+    preprocessed_ev_sig = []
+    for ev_sig in event_signals:
+        sig_length = len(ev_sig)
+        chunks = sig_length // window_size
+        fully_processed_ev_sig = np.zeros(chunks, dtype=np.float64)
+        prev_chunk = None
+        for i in range(chunks):
+            chunk = ev_sig[i*window_size : (i+1)*window_size]
+            proc_chunk = IoTCupidProcessing.ewma(chunk, prev_chunk, a)
+            deriv = IoTCupidProcessing.compute_derivative(proc_chunk)
+            fully_processed_ev_sig[i] = deriv
+            prev_chunk = proc_chunk
+        preprocessed_ev_sig.append(fully_processed_ev_sig)
+    return preprocessed_ev_sig
+        
+
+def process_events(events, event_signals, key_size, window_size, a, feature_dim, 
                    m_start, m_end, m_searches, mem_thresh, 
                    quantization_factor, cluster_sizes_to_check, cluster_th, Fs):
+
+    event_signals = preprocess_event_signals(event_signals, window_size, a)
 
     event_features = IoTCupidProcessing.get_event_features(
             event_signals, feature_dim