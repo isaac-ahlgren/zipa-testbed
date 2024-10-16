92defa57c784ec39f37432bf83b0e29bda738b3b
isaac-ahlgren <iahlgren@luc.edu>
2024-10-16 03:57:50 +0000

    some new plots, maybe?

--- eval/plot_scripts/plot_real_fuzzing_min_entropy.py
+++ eval/plot_scripts/plot_real_fuzzing_min_entropy.py
@@ -6,7 +6,7 @@ def min_entropy_devices(devices, contents, param_label, file_name_stub, fig_dir,
     for device in devices:
         device_id = device + "_bits"
 
-        device_file_stub = f"{file_name_stub}_{pairs[0]}_{pairs[1]}_{param_label}_minentropy"
+        device_file_stub = f"{file_name_stub}_{device}_{param_label}_minentropy"
 
         device_byte_list = extract_from_contents(contents, device_id)
 
@@ -21,7 +21,7 @@ def min_entropy_devices(devices, contents, param_label, file_name_stub, fig_dir,
         parameter_plot(min_entropy_list, param_list, f"{device} Using {param_label}", param_label, savefig=savefig, file_name=f"{device_file_stub}", fig_dir=fig_dir, ylabel="Min Entropy")
 
 def plot_schurmann(savefigs=True):
-    SCHURMANN_DATA_DIRECTORY = "../schurmann/schurmann_data/schurmann_real_fuzz"
+    SCHURMANN_DATA_DIRECTORY = "/home/isaac/ZIPA-TESTBED-DATA/schurmann_data/schurmann_real_fuzz"
     SCHURMANN_REAL_FUZZING_STUB = "schurmann_real_fuzz_day1"
     FIG_DIR_NAME_STUB = "schurmann_real_fuzz_min_entropy"
     
@@ -42,11 +42,11 @@ def plot_schurmann(savefigs=True):
     else:
         fig_dir = None
 
-    min_entropy_devices(DEVICE_PAIRS, contents, "window_length", SCHURMANN_REAL_FUZZING_STUB, fig_dir, savefig=savefigs)
-    min_entropy_devices(DEVICE_PAIRS, contents, "band_length", SCHURMANN_REAL_FUZZING_STUB, fig_dir, savefig=savefigs)
+    min_entropy_devices(DEVICES, contents, "window_length", SCHURMANN_REAL_FUZZING_STUB, fig_dir, savefig=savefigs)
+    min_entropy_devices(DEVICES, contents, "band_length", SCHURMANN_REAL_FUZZING_STUB, fig_dir, savefig=savefigs)
 
 def plot_miettinen(savefigs=True):
-    MIETTINEN_DATA_DIRECTORY = "../miettinen/miettinen_data/miettinen_real_fuzz"
+    MIETTINEN_DATA_DIRECTORY = "/home/isaac/ZIPA-TESTBED-DATA/miettinen_data/miettinen_real_fuzz"
     MIETTINEN_REAL_FUZZING_STUB = "miettinen_real_fuzz_day1"
     FIG_DIR_NAME_STUB = "miettinen_real_fuzz_min_entropy"
     
@@ -67,10 +67,10 @@ def plot_miettinen(savefigs=True):
     else:
         fig_dir = None
 
-    min_entropy_devices(DEVICE_PAIRS, contents, "f_samples", MIETTINEN_REAL_FUZZING_STUB, fig_dir, savefig=savefigs)
-    min_entropy_devices(DEVICE_PAIRS, contents, "w_samples", MIETTINEN_REAL_FUZZING_STUB, fig_dir, savefig=savefigs)
-    min_entropy_devices(DEVICE_PAIRS, contents, "rel_thr", MIETTINEN_REAL_FUZZING_STUB, fig_dir, savefig=savefigs)
-    min_entropy_devices(DEVICE_PAIRS, contents, "abs_thr", MIETTINEN_REAL_FUZZING_STUB, fig_dir, savefig=savefigs)
+    min_entropy_devices(DEVICES, contents, "f_samples", MIETTINEN_REAL_FUZZING_STUB, fig_dir, savefig=savefigs)
+    min_entropy_devices(DEVICES, contents, "w_samples", MIETTINEN_REAL_FUZZING_STUB, fig_dir, savefig=savefigs)
+    min_entropy_devices(DEVICES, contents, "rel_thr", MIETTINEN_REAL_FUZZING_STUB, fig_dir, savefig=savefigs)
+    min_entropy_devices(DEVICES, contents, "abs_thr", MIETTINEN_REAL_FUZZING_STUB, fig_dir, savefig=savefigs)
 
 
 def main():
