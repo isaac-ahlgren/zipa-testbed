a04a7fd3e2d2f0b57b76c4526f3168c11e4c129e
isaac-ahlgren <isaacahlgren12@gmail.com>
2024-10-22 15:52:40 -0500

    god im bad

--- src/signal_processing/fastzip.py
+++ src/signal_processing/fastzip.py
@@ -339,7 +339,7 @@ class FastZIPProcessing:
             fp = ""
             for pt in pts:
                 for index in pt:
-                    if chunk[index] > qs_thr:
+                    if chunk[int(index)] > qs_thr:
                         fp += "1"
                     else:
                         fp += "0"
