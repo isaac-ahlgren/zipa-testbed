5800f9206428f6c3932f54907807313ffac09e81
isaac-ahlgren <isaacahlgren12@gmail.com>
2024-10-27 18:25:51 -0500

    accidentally made a mistake

--- eval/plot_scripts/plot_event_histogram.py
+++ eval/plot_scripts/plot_event_histogram.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 from utils import parse_eval_directory_time_stamps, make_plot_dir, extract_from_contents, get_num_events_list
 
-def event_hist_plot(devices, contents, param1, param2, param1_range, param2_range, file_name_stub, savefigs=True, fig_dir=None, file_name=None):
+def event_hist_plot(devices, contents, param1, param2, param1_range, param2_range, savefigs=True, fig_dir=None, file_name=None):
     for device in devices:
         device_id = device + "_time_stamps"
 
