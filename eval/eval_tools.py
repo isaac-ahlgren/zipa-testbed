4158084861d9598166ddcf7f92da77479312c00a
isaac-ahlgren <isaacahlgren12@gmail.com>
2024-11-07 19:17:32 -0600

    didn't add this for some reason

--- eval/eval_tools.py
+++ eval/eval_tools.py
@@ -166,7 +166,7 @@ def load_signal_files(
 
 
 def load_events(file):
-    output = np.loadtxt(file, delimiter=',', skiprows=1, usecols=(1, 2))
+    output = np.loadtxt(file, delimiter=',', skiprows=1, usecols=(1, 2)).astype(np.uint32)
     return output
 
 
